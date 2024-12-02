40b890a9e94c8f25d0095fbcadabeeb1ba263c47
David Plass <dplassgit@users.noreply.github.com>
2024-12-01 20:56:16 -0500

    Initial commit

---
+++ src/com/plasstech/aocomp/driver/Java2C.java
@@ -0,0 +1,12 @@
+package com.plasstech.aocomp.driver;
+
+public class Java2C {
+
+  public static void main(String args[]) {
+    // TODO: instantiate the lexer & parser, read from stdin, and write to stdout
+    System.out.println("   .globl main");
+    System.out.println("main:");
+    System.out.println("   movl $2, %eax");
+    System.out.println("   ret");
+  }
+}
