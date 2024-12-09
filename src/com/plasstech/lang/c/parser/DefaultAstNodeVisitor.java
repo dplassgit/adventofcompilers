d28257b1ef75ad9587fc449a6befa26be90f0740
David Plass <dplassgit@users.noreply.github.com>
2024-12-09 17:25:45 -0500

    Simplified DefaultAstNodeVisitor

--- src/com/plasstech/lang/c/parser/DefaultAstNodeVisitor.java
+++ src/com/plasstech/lang/c/parser/DefaultAstNodeVisitor.java
@@ -1,29 +1,3 @@
 package com.plasstech.lang.c.parser;
 
-public class DefaultAstNodeVisitor implements AstNodeVisitor<Void> {
-
-  @Override
-  public <T> Void visit(Constant<T> n) {
-    return null;
-  }
-
-  @Override
-  public Void visit(FunctionDef n) {
-    return null;
-  }
-
-  @Override
-  public Void visit(Program n) {
-    return null;
-  }
-
-  @Override
-  public Void visit(Return n) {
-    return null;
-  }
-
-  @Override
-  public Void visit(UnaryExp n) {
-    return null;
-  }
-}
+public abstract class DefaultAstNodeVisitor extends GenericNodeVisitor<Void> {}
