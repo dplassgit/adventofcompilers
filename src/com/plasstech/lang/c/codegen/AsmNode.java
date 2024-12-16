dbbd8ba79dab5e202499c24c085b0cadee2339cd
David Plass <dplassgit@users.noreply.github.com>
2024-12-16 09:56:22 -0500

    Update AsmNode.java

--- src/com/plasstech/lang/c/codegen/AsmNode.java
+++ src/com/plasstech/lang/c/codegen/AsmNode.java
@@ -1,34 +1,37 @@
 package com.plasstech.lang.c.codegen;
 
+/** 
+ * Represents a "node" in the Asm tree.
+ * <p>
+ * Some nodes implement the `Instruction` interface, which extends `AsmNode`.
+ */
 public interface AsmNode {
   interface Visitor<R> {
-    R visit(AsmProgramNode n);
+    R visit(AllocateStack n);
 
     R visit(AsmFunctionNode n);
 
-    R visit(Mov n);
+    R visit(AsmProgramNode n);
+
+    R visit(AsmBinary n);
 
     R visit(AsmUnary n);
 
-    R visit(AsmBinary n);
+    R visit(Cdq n);
 
     R visit(Cmp n);
 
     R visit(Idiv n);
 
-    R visit(Cdq n);
-
     R visit(Jmp n);
 
     R visit(JmpCC n);
 
-    R visit(SetCC n);
-
-    R visit(Label n);
-
-    R visit(AllocateStack n);
+    R visit(Mov n);
 
     R visit(Ret n);
+
+    R visit(SetCC n);
   }
 
   <R> R accept(Visitor<R> visitor);
