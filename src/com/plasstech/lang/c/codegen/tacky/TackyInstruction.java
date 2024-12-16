2e50695a52b739f681651830e459993baaa35f03
David Plass <dplassgit@users.noreply.github.com>
2024-12-16 09:57:17 -0500

    Update TackyInstruction.java

--- src/com/plasstech/lang/c/codegen/tacky/TackyInstruction.java
+++ src/com/plasstech/lang/c/codegen/tacky/TackyInstruction.java
@@ -5,21 +5,21 @@ package com.plasstech.lang.c.codegen.tacky;
  */
 interface TackyInstruction {
   interface Visitor<R> {
-    R visit(TackyUnary op);
-
     R visit(TackyBinary op);
 
     R visit(TackyCopy op);
 
     R visit(TackyJump op);
 
-    R visit(TackyJumpZero op);
-
     R visit(TackyJumpNotZero op);
+
+    R visit(TackyJumpZero op);
 
     R visit(TackyLabel op);
 
     R visit(TackyReturn op);
+
+    R visit(TackyUnary op);
   }
 
   <R> R accept(Visitor<R> visitor);
