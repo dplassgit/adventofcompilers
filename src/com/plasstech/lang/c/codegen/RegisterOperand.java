0c92073ed3cf66672fcfe673651d69b56a251806
David Plass <dplassgit@users.noreply.github.com>
2025-03-31 18:42:21 -0400

    Refactor/simplify RegisterOperand

--- src/com/plasstech/lang/c/codegen/RegisterOperand.java
+++ src/com/plasstech/lang/c/codegen/RegisterOperand.java
@@ -4,81 +4,54 @@ import java.util.List;
 
 import com.google.common.collect.ImmutableList;
 
-public record RegisterOperand(Register register) implements Operand {
-  private enum Register {
-    RSP,
-    RAX("al", "eax"),
-    RCX("cl", "ecx"),
-    RDX("dl", "edx"),
-    RDI("dil", "edi"),
-    RSI("sil", "esi"),
-    R8("r8"),
-    R9("r9"),
-    R10("r10"),
-    R11("r11"),
-    XMM0,
-    XMM1,
-    XMM2,
-    XMM3,
-    XMM4,
-    XMM5,
-    XMM6,
-    XMM7,
-    XMM14,
-    XMM015;
-
-    private final String string1;
-    private final String string4;
-
-    Register(String name) {
-      this.string1 = name + "b";
-      this.string4 = name + "d";
-    }
-
-    Register(String string1, String string4) {
-      this.string1 = string1;
-      this.string4 = string4;
-    }
-
-    Register() {
-      this.string1 = name();
-      this.string4 = name();
-    }
-
-    String toString(int bytes) {
-      if (bytes == 1) {
-        return string1;
-      }
-      if (bytes == 4) {
-        return string4;
-      }
-      // 8 bytes
-      return name();
-    }
+public record RegisterOperand(String name, String name1, String name4) implements Operand {
+  public RegisterOperand(String name) {
+    this(name, name, name);
   }
 
-  public static RegisterOperand RAX = new RegisterOperand(Register.RAX);
-  public static RegisterOperand RCX = new RegisterOperand(Register.RCX);
-  public static RegisterOperand RDX = new RegisterOperand(Register.RDX);
-  public static RegisterOperand RDI = new RegisterOperand(Register.RDI);
-  public static RegisterOperand RSI = new RegisterOperand(Register.RSI);
-  public static RegisterOperand R8 = new RegisterOperand(Register.R8);
-  public static RegisterOperand R9 = new RegisterOperand(Register.R9);
-  public static RegisterOperand R10 = new RegisterOperand(Register.R10);
-  public static RegisterOperand R11 = new RegisterOperand(Register.R11);
-  public static RegisterOperand RSP = new RegisterOperand(Register.RSP);
+  public static RegisterOperand RAX = new RegisterOperand("RAX", "AL", "EAX");
+  public static RegisterOperand RCX = new RegisterOperand("RCX", "CL", "ECX");
+  public static RegisterOperand RDX = new RegisterOperand("RDX", "DL", "EDX");
+  public static RegisterOperand RDI = new RegisterOperand("RDI", "DIL", "EDI");
+  public static RegisterOperand RSI = new RegisterOperand("RSI", "SIL", "ESI");
+  public static RegisterOperand R8 = new RegisterOperand("R8", "R8b", "R8d");
+  public static RegisterOperand R9 = new RegisterOperand("R9", "R9b", "R9d");
+  public static RegisterOperand R10 = new RegisterOperand("R10", "R10b", "R10d");
+  public static RegisterOperand R11 = new RegisterOperand("R11", "R11b", "R11d");
+  public static RegisterOperand RSP = new RegisterOperand("RSP");
+  public static RegisterOperand XMM0 = new RegisterOperand("XMM0");
+  public static RegisterOperand XMM1 = new RegisterOperand("XMM1");
+  public static RegisterOperand XMM2 = new RegisterOperand("XMM2");
+  public static RegisterOperand XMM3 = new RegisterOperand("XMM3");
+  public static RegisterOperand XMM4 = new RegisterOperand("XMM4");
+  public static RegisterOperand XMM5 = new RegisterOperand("XMM5");
+  public static RegisterOperand XMM6 = new RegisterOperand("XMM6");
+  public static RegisterOperand XMM7 = new RegisterOperand("XMM7");
+  public static RegisterOperand XMM14 = new RegisterOperand("XMM14");
+  public static RegisterOperand XMM15 = new RegisterOperand("XMM15");
 
   public static final List<RegisterOperand> ARG_REGISTERS = ImmutableList.of(
       RDI, RSI, RDX, RCX, R8, R9);
 
+  private String nameToString(int bytes) {
+    if (bytes == 1) {
+      return name1;
+    }
+    if (bytes == 4) {
+      return name4;
+    }
+    // 8 bytes
+    return name;
+  }
+
   @Override
   public String toString() {
-    return "%" + register.toString(4);
+    return toString(4);
   }
 
   @Override
   public String toString(int bytes) {
-    return "%" + register.toString(bytes);
+    return "%" + nameToString(bytes);
   }
 
   @Override
